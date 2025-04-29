9a2384fbeb340ea1462cc2c3e0ce0ec547c6e566
Orson DESSEIN <odessein@paul-f3Ar8s2.clusters.42paris.fr>
2025-04-29 14:54:24 +0200

    check_base + macro atoi_base

--- ft_atoi_base.s
+++ ft_atoi_base.s
@@ -8,79 +8,90 @@ section .text
 ;a -> 0 * 16 + pos a (10) = 10
 ;a -> 10 * 16 + pos a (10) = 170
 ;f -> 170 * 16 + pos f (15) = 2720 + 15 2735
-;
-;len(base) = 16 (dans ce cas)
-;faut revert la string
-;
-;a -> 0 * 16 + pos a (10) = 10
-;a -> 10 * 16 + pos a (10) = 170
-;f -> 170 * 16 + pos f (15) = 2720 + 15 2735
-;
 
-;rsi *str
-;rdi *base
+;rdi *str
+;rsi *base
+
+%macro CLEAR_REG 1
+	xor %1, %1
+%endmacro
+
+%macro CHECK_ZERO 2 ; 2 is the number of arguments
+	cmp %1, 0
+	jz %2
+%endmacro
+
+%macro CMP_A_B_JMP 3
+	cmp %1, %2
+	jz %3
+%endmacro
+;%%label is the label syntax in macro
 
 ft_atoi_base:
-	push rbp
-	mov rbp, rsp
-	sub rsp, 32
 
-	xor rdi, rdi
-	jmp check_base
-
-
-	add rsp, 32
-	mov rsp, rbp
-	pop rbp
+	mov rdi, rsi 
+	call check_base
+	; Possible to make a macro on the test
+	CMP_A_B_JMP rax, 1, .err
 	ret
 
 
 ; case emtpy base ou len(base) = 1
 ; base has duplicate character
 ; base containe whitespace or "+" et "-"
-.err:
-	mov rax, 0
-	ret
+	.err:
+		mov rax, 0
+		ret
 
 check_base:
 	; $rsi c'est ma base
 	; loop sur la base
 	; lodsb load si dans al et increment esi
+	CLEAR_REG ecx
 .check_base.loop:
 	lodsb
-	cmp al, 32
-	jz .check_base.end_bad
-	cmp al, 43
-	jz .check_base.end_bad
-	cmp al, 45
-	jz .check_base.end_bad
-	test rdi, rdi
-	jz  .continue_loop
+	CHECK_ZERO al, .check_base.end_good
+	CMP_A_B_JMP al, 32, .check_base.end_bad		; check ' '
+	CMP_A_B_JMP al, 43, .check_base.end_bad		; check '+'
+	CMP_A_B_JMP al, 45, .check_base.end_bad		; check '-'
+	add ecx, 1
 	push rsi
-	mov rsi, rdi ; CETTE LIGNE OUIII
-	mov r13, rax
+	push rcx
+	mov rsi, rdi ; copy that start address of base into rsi
+	mov r13, rax ; store dans r13b al
+	mov edx, ecx ; edx prend le current index
+	sub edx, 1 ; max = current_index - 1
+	CLEAR_REG ecx
+	.loop_duplicate:			; Loop on the base to check if current char as not duplicate
+		lodsb
 
-	
-	.loop_duplicate:
-	lodsb		; parcours la liste rdi (du a la ligne avant) qui est la chaine de charachter des character parcouru
-	cmp al, r13b ; cmp that actual char to all the previous one
-	jz .check_base.end_bad
-	jnz .loop_duplicate
+		cmp ecx, edx
 
-	mov al, r13b
-	pop rsi
+		jz  .end_loop_duplicate
 
-	.continue_loop:
-	; for this logic of storing the char into another string is a bit overkill, i should just rebrowse to currentIdx - 1
-	; but its possible to allocate string space in the bss section, to use is a reference (not advised)
-	stosb	; stor the current char into rdi
-	
-	mov rax, 0
-	ret
+		cmp al, r13b ; cmp that actual char to all the previous one
+		jz .loop_duplicate.bad
+
+		add ecx, 1
+		jmp .loop_duplicate
+
+		.end_loop_duplicate:
+			pop rcx
+			pop rsi
+			jmp .check_base.loop
+		
+
+		.loop_duplicate.bad:
+			pop rcx
+			pop rsi
+		; then go to end bad
 
 	.check_base.end_bad:
-	mov rax, 1
-	ret
+		mov rax, 1
+		ret
 
+	.check_base.end_good
+		mov rax, 0
+		ret
 
 
--- test_atoi.c
+++ test_atoi.c
@@ -1,4 +1,5 @@
 #include "libasm.h"
+#include <stdio.h>
 
 int main() {
 
